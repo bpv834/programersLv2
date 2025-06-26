fd67c9aac23a4d117e11d2c8fc5d06f02a7a6756
bpv834 <yungjoon2@naver.com>
2025-06-26 22:48:45 +0900

    괄호 변환, 문자열 자르기, dfs

---
+++ transe_parentheses/src/TranseParentheses.kt
@@ -0,0 +1,69 @@
+import java.util.*
+class Solution {
+    fun solution(p: String): String {
+
+        return dfs(p)
+    }
+
+    fun dfs(str : String):String{
+        if(str == "") return str
+
+        var u = str
+        var v = ""
+        var subStr = ""
+        for(i in 2 until str.length step 2){
+            subStr = str.substring(0,i)
+            if(getParenthesesCnt(subStr,'(') == getParenthesesCnt(subStr,')')){
+                u= subStr
+                v= str.drop(i)
+                break
+            }
+        }
+        // 올바른 문자열이라면
+        if(isBalanceParentheses(u)){
+            return u + dfs(v)
+        }else{
+            // 그렇지 않다면
+            var emptyStr = "("
+            emptyStr += dfs(v) +")"
+            //앞, 끝 글자 제거
+            u = u.substring(1,u.length-1)
+            val reverseU = reverseParentheses(u)
+            return emptyStr + reverseU
+
+        }
+
+
+    }
+    // '(' 개수 새는 메서드
+    fun getParenthesesCnt(str : String, ch : Char): Int{
+        var cnt = 0
+        for(idx in str.indices){
+            if(str.get(idx)==ch) cnt++
+        }
+        return cnt
+    }
+    // 균형잡힌 괄호인가?
+    fun isBalanceParentheses(str : String): Boolean{
+        val stack = Stack<Char>()
+
+        for(i in str.indices){
+            if(str.get(i)=='(') stack.push('(')
+            else{
+                if(!stack.isEmpty())
+                    stack.pop()
+            }
+        }
+        if(stack.isEmpty()) return true
+        return false
+    }
+
+    fun reverseParentheses(u : String):String{
+        val sb = StringBuilder()
+        for(i in u.indices){
+            if(u.get(i)=='(') sb.append(')')
+            else sb.append('(')
+        }
+        return sb.toString()
+    }
+}
