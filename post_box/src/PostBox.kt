67b7fe6a3cbe43a6f7e0561604088aaade15712b
bpv834 <yungjoon2@naver.com>
2025-07-22 23:39:28 +0900

    택배상자/스택에 넣고 조건에 맞을때까지 반복적으로 빼고 반복작업후 다시 넣는다

---
+++ post_box/src/PostBox.kt
@@ -0,0 +1,17 @@
+import java.util.*
+class Solution {
+    fun solution(order: IntArray): Int {
+        var answer: Int = 0
+        val stack = Stack<Int>()
+        var idx=0
+        for(i in 1 .. order.size){
+            stack.push(i)
+            while(!stack.isEmpty()&&order[idx] == stack.peek()){
+                idx++
+                stack.pop()
+                answer++
+            }
+        }
+        return answer
+    }
+}
