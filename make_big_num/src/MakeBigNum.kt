40747900cf7ca3a8c79876ca71c9393a99ad225c
bpv834 <yungjoon2@naver.com>
2025-08-01 03:34:57 +0900

    큰 수 만들기/ 조건에 걸리지 않거나 제거를 모자라게 했을때 그만큼 더 빼줘야함

--- make_big_num/src/MakeBigNum.kt
+++ make_big_num/src/MakeBigNum.kt
@@ -1,6 +1,24 @@
+import java.util.*
 class Solution {
     fun solution(number: String, k: Int): String {
-        var answer = ""
-        return answer
+        val chs = number.toCharArray()
+        val stack = Stack<Pair<Int,Int>>()
+        var cnt=0
+
+        for(i in number.indices){
+            while(!stack.isEmpty() &&stack.peek().first < number[i].toInt()-48){
+                val pop = stack.pop()
+                chs[pop.second] = '-'
+                cnt++
+                if(cnt == k) break
+            }
+            if(cnt == k) break
+            stack.push(number[i].toInt()-48 to i)
+        }
+        val result = chs.joinToString("").replace("-","")
+        val left = k-cnt
+        if(cnt != k) return result.substring(0,result.length-left)
+        return result
+
     }
 }
