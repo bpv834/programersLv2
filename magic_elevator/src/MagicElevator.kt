fbe242df139d2aa6e2840a6994ddd40f0bcfd8f3
bpv834 <yungjoon2@naver.com>
2025-08-05 22:28:34 +0900

    마법의 엘리베이터

---
+++ magic_elevator/src/MagicElevator.kt
@@ -0,0 +1,6 @@
+class Solution {
+    fun solution(storey: Int): Int {
+        var answer: Int = 0
+        return answer
+    }
+}
--- sub_sequense_sum/src/SubSequenceSum.kt
+++ sub_sequense_sum/src/SubSequenceSum.kt
@@ -1,28 +1,40 @@
-import java.util.*
 class Solution {
     fun solution(sequence: IntArray, k: Int): IntArray {
-        val q = LinkedList<Pair<Int,Int>>()
-        var answer = IntArray(2)
-        var sum =0
-        var min = Int.MAX_VALUE
-        for(i in sequence.indices){
-            // println(sum)
-            while(!q.isEmpty()&& sum + sequence[i] > k){
-                val st = q.poll()
-                sum-=st.first
-            }
-            q.add(Pair(sequence[i],i))
-            sum+=sequence[i]
+        var answer: IntArray = IntArray(2)
+        var min =Int.MAX_VALUE
+        var sum = sequence[0]
+        var lt=0
+        var rt=1
 
-            if(sum == k && q.size < min){
-                min = q.size
-                val st = q.poll()
-                //  println("st = $st")
-                sum-=st.first
-                answer[0]=st.second
-                answer[1] =i
+        while(lt<rt && rt<sequence.size){
+            println(sum)
+            when{
+                sum > k->{
+                    sum-=sequence[lt++]
+                }
+
+                sum < k->{
+                    sum+=sequence[rt++]
+                }
+
+                sum == k ->{
+                    if(rt-lt+1 < min){
+                        min = rt-lt+1
+                        answer[0]=lt
+                        answer[1]=rt
+                    }
+                }
             }
+
         }
+
+
         return answer
     }
+
+}
+
+fun main() {
+    val so = Solution().solution(intArrayOf(1,2,3,4,5),7)
+
 }
