1b9f898215e3f095b06c8040eb694f0e527aa2f4
bpv834 <yungjoon2@naver.com>
2025-08-07 22:10:30 +0900

    마법의 엘리베이터/ 5인경우 항상 올리거나 항상 내리면 안됨 555를 생각해보자

--- magic_elevator/src/MagicElevator.kt
+++ magic_elevator/src/MagicElevator.kt
@@ -1,6 +1,22 @@
 class Solution {
+    var minCnt = Int.MAX_VALUE
     fun solution(storey: Int): Int {
         var answer: Int = 0
-        return answer
+        dfs(storey,0)
+        return minCnt
+    }
+    fun dfs(num : Int , cnt : Int){
+        if(num == 0){
+            minCnt = Math.min(minCnt,cnt)
+        }
+        if(cnt >= minCnt) return
+
+        val end = num%10
+        val new = num/10
+
+        // 내림
+        dfs(new, cnt+end)
+        // 올림
+        dfs(new+1,cnt+10-end)
     }
 }
